823693e725f8bfc0f00bd6fa3ec8cd0221055126
Poncho <aimyoyo@gmail.com>
2024-09-06 21:11:09 -0600

    Connected database.

--- src/app.py
+++ src/app.py
@@ -1,12 +1,25 @@
-from flask import Flask, render_template, redirect, url_for, request
-import logging
+from flask              import Flask, render_template, redirect, url_for, request
+from flask_sqlalchemy   import SQLAlchemy
+from sqlalchemy.sql     import text
+import logging, os
 
 # because logging is better!
 logging.basicConfig(format = '%(name)s@{asctime} : %(levelname)s : %(message)s',
                     datefmt = '%y%m%d_%H:%M',
                     level = logging.DEBUG)
 
+# Creating a naming convention for db access
+db = SQLAlchemy()
+
+# Starting the app
 app = Flask(__name__)
+
+# Establishing database connection and initializing
+db_name                                      = str(os.path.abspath('data/quotes.db'))
+app.config['SQLALCHEMY_DATABASE_URI']        = 'sqlite:///' + db_name
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+# db.init_app(app)
 
 @app.route("/")
 def index():
@@ -25,4 +38,5 @@ def add_author():
         if(authShort and authLong):
             notification = 'was sucessfully included!'
             return render_template('submit_author.html', notification = notification, authLong = authLong)
-    return render_template('submit_author.html')
+    else:
+        return render_template('submit_author.html')
