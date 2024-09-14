1ec7de66b78ed8f0bb05fcbe7a3e9c3cf5fc4b8d
Poncho <aimyoyo@gmail.com>
2024-09-14 10:30:55 -0600

    Added submit_publishers.html methods

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
-from flask              import Flask, render_template, redirect, url_for, request
+from flask              import Flask, render_template, redirect, url_for, g, request
 from flask_sqlalchemy   import SQLAlchemy
 from sqlalchemy.sql     import text
-import logging, os
+import logging, os, sqlite3
 
 # because logging is better!
 logging.basicConfig(format = '%(name)s@{asctime} : %(levelname)s : %(message)s',
@@ -9,17 +9,33 @@ logging.basicConfig(format = '%(name)s@{asctime} : %(levelname)s : %(message)s',
                     level = logging.DEBUG)
 
 # Creating a naming convention for db access
-db = SQLAlchemy()
+# db = SQLAlchemy()
 
 # Starting the app
 app = Flask(__name__)
 
 # Establishing database connection and initializing
-db_name                                      = str(os.path.abspath('data/quotes.db'))
-app.config['SQLALCHEMY_DATABASE_URI']        = 'sqlite:///' + db_name
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db = SQLAlchemy(app)
-# db.init_app(app)
+db_name = os.path.abspath('data/quotes.db')
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if(db is None):
+        db = g._database = sqlite3.connect(db_name)
+    return db
+
+def query_db(query: str, args = (), one = False):
+    c = get_db().execute(query, args)
+    f = c.fetchall()
+    get_db().commit()
+    c.close()
+    return (f[0] if f else None) if one else f
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if(db is not None):
+        db.close()
+    return 0
 
 @app.route("/")
 def index():
@@ -32,11 +48,41 @@ def add_author():
         notification = None
         logging.debug((authShort, authLong))        
 
-        # TODO - Add them to the database
-        #      - Return them into the page -> Wikipedia entry about them?
-        #      - Added!
+        # TODO - Return them into the page -> Wikipedia entry about them?
+        # Insert only if both fields have been fulfilled, otherwise ask for them
         if(authShort and authLong):
-            notification = 'was sucessfully included!'
-            return render_template('submit_author.html', notification = notification, authLong = authLong)
+            try:
+                query_db('INSERT INTO author(display_name, full_name) VALUES (?, ?);', (authShort, authLong))
+                notification = 'was sucessfully included!'
+                return render_template('submit_author.html', notification = notification, authLong = authLong)
+            except Exception as e:
+                # TODO Beautify
+                return f'<p>{e}</p>'
+        else:
+            # TODO
+            pass
     else:
         return render_template('submit_author.html')
+
+@app.route("/add/publisher", methods = ['POST', 'GET'])
+def add_publisher():
+    if(request.method == 'POST'):
+        pubName = request.form['name']
+        notification = None
+        logging.debug(pubName)        
+
+        # TODO - Return them into the page -> Wikipedia entry about them?
+        # Insert only if both fields have been fulfilled, otherwise ask for them
+        if(pubName):
+            try:
+                query_db('INSERT INTO publisher(name) VALUES (?);', (pubName,))
+                notification = 'was sucessfully included!'
+                return render_template('submit_publisher.html', notification = notification, pubName = pubName)
+            except Exception as e:
+                # TODO Beautify
+                return f'<p>{e}</p>'
+        else:
+            # TODO
+            pass
+    else:
+        return render_template('submit_publisher.html')
--- src/data/create_tables.sql
+++ src/data/create_tables.sql
@@ -1,5 +1,10 @@
+DROP TABLE author;
+DROP TABLE publisher;
+DROP TABLE book;
+DROP TABLE quote;
+
 CREATE TABLE IF NOT EXISTS author(
-    id                  INT  PRIMARY KEY,
+    id                  INTEGER PRIMARY KEY AUTOINCREMENT,
     display_name        TEXT NOT NULL,
     full_name           TEXT NOT NULL,
     living_years        TEXT, -- TODO: IMPLEMENT
@@ -7,28 +12,27 @@ CREATE TABLE IF NOT EXISTS author(
 );
 
 CREATE TABLE IF NOT EXISTS publisher(
-    id                  INT  PRIMARY KEY,
+    id                  INTEGER PRIMARY KEY AUTOINCREMENT,
     name                TEXT NOT NULL
 );
 
 -- CREATE TABLE IF NOT EXISTS alternate_publisher();
 
 CREATE TABLE IF NOT EXISTS book(
-    id                  INT  PRIMARY KEY,
-    author_id           INT  NOT NULL,
-    year                INT,
+    id                  INTEGER PRIMARY KEY AUTOINCREMENT,
+    year                INTEGER,
     title               TEXT NOT NULL,
-    publisher_id        INT  NOT NULL, 
-    edition             INT,
+    author_id           INTEGER  NOT NULL, 
+    publisher_id        INTEGER  NOT NULL, 
+    edition             INTEGER,
     description         TEXT, -- TODO: ADD OTHER STUFF?
     FOREIGN KEY(author_id) REFERENCES author(id),
     FOREIGN KEY(publisher_id) REFERENCES publisher(id)
 );
 
 CREATE TABLE IF NOT EXISTS quote(
-    id                  INT  PRIMARY KEY,
-    book_id             INT,
-    author_id           INT  NOT NULL,
+    id                  INTEGER PRIMARY KEY AUTOINCREMENT,
+    author_id           INTEGER NOT NULL,
     quote               TEXT NOT NULL,
-    year                INT
+    year                INTEGER
 );
